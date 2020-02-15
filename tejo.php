5535df3a9312a6189f1c604cf1a894606a7c5969
hafidteja778 <58174605+hafidteja778@users.noreply.github.com>
2020-02-15 21:33:25 +0700

    tejo.php

--- tejo.php
+++ tejo.php
@@ -65,7 +65,7 @@ echo " ===========================\n";
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
-        goto;
+        goto goride
         sleep(3);
         }
         $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
