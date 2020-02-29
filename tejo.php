a2bfea96bbf2f2fb54116ee63453b793dda206c3
hafidteja778 <58174605+hafidteja778@users.noreply.github.com>
2020-02-29 13:04:15 +0700

    tejo.php

--- tejo.php
+++ tejo.php
@@ -59,8 +59,7 @@ echo " ===========================\n";
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
-        sleep(1);
-        }
+        sleep(3);
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai'))
